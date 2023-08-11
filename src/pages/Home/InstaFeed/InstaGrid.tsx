52efea59106247bedb5cbece680faa1abcb3990a
RyanJSmillie <RyanJSmillie@hotmail.co.uk>
2023-08-11 09:04:46 +0200

    Added types to insta feed component

--- src/pages/Home/InstaFeed/InstaGrid.tsx
+++ src/pages/Home/InstaFeed/InstaGrid.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
+import { InstaGridProps } from "../../../interfaces/InstaFeed";
+
 import "./InstaGrid.css";
 
-const InstaGrid = ({ items }) => {
+const InstaGrid = ({ items }: InstaGridProps) => {
   if (items.length === 0) {
     return <div>{new Array(3).fill(<h1>Placeholder</h1>)}</div>;
   }
 
   return (
     <div className="grid-style">
-      {items.map(item => {
-        return <img className="item-style" src={item.mediaUrl} alt="instagram" />
+      {items.map((item) => {
+        return (
+          <img className="item-style" src={item.mediaUrl} alt="instagram" />
+        );
       })}
     </div>
   );
