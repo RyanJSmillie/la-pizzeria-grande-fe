ac9a683f73089f8e4d90660ca04c5a0be32dd14f
RyanJSmillie <RyanJSmillie@hotmail.co.uk>
2023-08-11 15:34:58 +0200

    Add firebase database integration

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,30 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Menu.css";
 
 import ListItem from "../../components/ListItem/ListItem";
 
-import image1 from "../../assets/pizza-1.jpeg";
-import image2 from "../../assets/pizza-4.jpeg";
-
-const menuData = [
-  {
-    title: "OG Cheese (V)",
-    description:
-      "The New York classic. Simply: house tomato sauce, mozzarella blend, finished with evoo and parm",
-    price: 10.5,
-    img: image1,
-  },
-  {
-    title: "Funghi Jambon *NEW*",
-    description:
-      "Roast ham and mushrooms run the show, backed with marinara sauce, garlic cream, mozzarella, oregano, black pepper, and chives.",
-    price: 14.5,
-    img: image2,
-  },
-];
-
 const Menu = () => {
+  const [menuData, setMenuData] = useState([]);
+
+  useEffect(() => {
+    const databaseUrl =
+      "https://la-pizzeria-grande-8def1-default-rtdb.europe-west1.firebasedatabase.app/Menu.json";
+
+    const doFetch = async () => {
+      const res: Response = await fetch(databaseUrl);
+      const json = await res.json();
+      setMenuData(json);
+    };
+
+    doFetch();
+  });
+
   return (
     <div className="section">
       <h1 className="heading-menu">Menu</h1>
