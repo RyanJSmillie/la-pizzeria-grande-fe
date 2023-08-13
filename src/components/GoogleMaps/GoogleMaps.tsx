6926c0d6a2bccad324e1d789fc851daa09aab102
RyanJSmillie <RyanJSmillie@hotmail.co.uk>
2023-08-13 20:32:46 +0200

    Add dark mode and associated changes to css

--- src/components/GoogleMaps/GoogleMaps.tsx
+++ src/components/GoogleMaps/GoogleMaps.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
@@ -9,6 +9,9 @@ import pizza from "../../assets/pizza-small.png";
 const googleMapsApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const GoogleMaps = () => {
+  const [mapState, setMapState] = useState(
+    document.documentElement.getAttribute("data-theme") || "light"
+  );
   const { isLoaded } = useLoadScript({
     googleMapsApiKey,
   });
@@ -16,6 +19,10 @@ const GoogleMaps = () => {
     () => ({ lat: 48.1350344049515, lng: 11.573813936839583 }),
     []
   );
+
+  window.addEventListener("click", () => {
+    setMapState(document.documentElement.getAttribute("data-theme"));
+  });
 
   return (
     <div className="App">
@@ -26,6 +33,101 @@ const GoogleMaps = () => {
           mapContainerClassName="map-container"
           center={center}
           zoom={10}
+          options={
+            mapState === "dark"
+              ? {
+                  styles: [
+                    {
+                      elementType: "geometry",
+                      stylers: [{ color: "#242f3e" }],
+                    },
+                    {
+                      elementType: "labels.text.stroke",
+                      stylers: [{ color: "#242f3e" }],
+                    },
+                    {
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#746855" }],
+                    },
+                    {
+                      featureType: "administrative.locality",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "poi",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "poi.park",
+                      elementType: "geometry",
+                      stylers: [{ color: "#263c3f" }],
+                    },
+                    {
+                      featureType: "poi.park",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#6b9a76" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "geometry",
+                      stylers: [{ color: "#38414e" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "geometry.stroke",
+                      stylers: [{ color: "#212a37" }],
+                    },
+                    {
+                      featureType: "road",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#9ca5b3" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "geometry",
+                      stylers: [{ color: "#746855" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "geometry.stroke",
+                      stylers: [{ color: "#1f2835" }],
+                    },
+                    {
+                      featureType: "road.highway",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#f3d19c" }],
+                    },
+                    {
+                      featureType: "transit",
+                      elementType: "geometry",
+                      stylers: [{ color: "#2f3948" }],
+                    },
+                    {
+                      featureType: "transit.station",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#d59563" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "geometry",
+                      stylers: [{ color: "#17263c" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "labels.text.fill",
+                      stylers: [{ color: "#515c6d" }],
+                    },
+                    {
+                      featureType: "water",
+                      elementType: "labels.text.stroke",
+                      stylers: [{ color: "#17263c" }],
+                    },
+                  ],
+                }
+              : {styles: []}
+          }
         >
           <Marker
             position={{ lat: 48.1350344049515, lng: 11.573813936839583 }}
